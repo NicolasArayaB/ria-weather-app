04b7e93d31f8d3dc24697ecacfddc8dce2b14ae1
Nico <nicolasaraya@gmail.com>
2021-08-06 21:13:19 -0400

    Created api call

--- src/Store/Flux.js
+++ src/Store/Flux.js
@@ -1,7 +1,29 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {},
-    action: {}
+  return {
+    store: {
+      hourlyForecast: [],
+      dailyForecast: [],
+    },
+
+    actions: {
+      getHourlyForecast: async (city, hours) => {
+        const url = 'https://api.weatherbit.io/v2.0/forecast/hourly?city=' + city + '&hours=' + hours + '&key=cb821d89227f4406bed149794eb205fa';
+
+        const request = await fetch(url);
+        const data = await request.json();
+
+        let store = getStore().hourlyForecast;
+        setStore({ hourlyForecast: {...store, [city]: data} })
+      },
+
+      getDailyForecast: async (city, days) => {
+        const url = 'https://api.weatherbit.io/v2.0/forecast/daily?days=' + days + '&city=' + city + '&key=cb821d89227f4406bed149794eb205fa';
+        const request = await fetch(url);
+        const data = await request.json();
+        let store = getStore().dailyForecast;
+        setStore({ dailyForecast: {...store, [city]: data} })
+      }
+    }
   }
 }
 export default getState;
