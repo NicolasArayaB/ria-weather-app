86c823b8a6db05c51f5de7be7d3bfc8b165ddf4d
Nico <nicolasaraya@gmail.com>
2021-08-08 20:26:31 -0400

    Added home link to logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,23 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const TitleWrapper = styled.section `
   align-items: center;
   background-color: #1181B2;
   display: flex;
+  flex-direction: row;
+  
   img {
+    justify-content: flex-start;
+    margin-bottom: 30px;
+    max-width: 7em;
+    padding: 15px 0 0 15px;
+    width: 100%;
+    
     @media(max-width: 500px){
       width: 5em;
-  };
-  justify-content: flex-start;
-  margin-bottom: 30px;
-  max-width: 7em;
-  padding: 15px 0 0 15px;
-  width: 100%;
+    }    
+  }
 `;
 
 const Title = styled.h1 `
@@ -24,8 +29,10 @@ const Title = styled.h1 `
 
 const Header = () => {
   return(
-    <TitleWrapper>                 
-      <img alt='Weather logo' src='https://res.cloudinary.com/dkjaz9pdx/image/upload/v1628439274/samples/newLogo_kadqqg.png' />
+    <TitleWrapper>
+      <Link to='/'>
+        <img alt='Weather logo' src='https://res.cloudinary.com/dkjaz9pdx/image/upload/v1628439274/samples/newLogo_kadqqg.png' />
+      </Link>
       <Title>Weather App</Title>
     </TitleWrapper>
   )
