015821b3f6157d1e114ce9b43bd9038b27edc020
Nico <nicolasaraya@gmail.com>
2021-11-21 14:40:09 -0300

    Deploy

--- src/Components/DailyCard.js
+++ src/Components/DailyCard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect }  from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router';
 
 import { Context } from '../Store/AppContext';
@@ -11,14 +11,14 @@ import { TableWrapper } from '../Styles/DailyCard';
 const DailyCard = (props) => {
   const { actions, store } = useContext(Context);
   const params = useParams();
-  
+
   let localForecast = store.dailyForecast[props.city] ? store.dailyForecast[props.city].data : null;
 
   const getIcon = (icon) => 'https://www.weatherbit.io/static/img/icons/' + icon + '.png'
-  const forecast = () => actions.getDailyForecast(props.city, props.days || 5);
   const cleanDate = (index) => (localForecast[index].valid_date).split('-').reverse().join('/');
 
   useEffect(() => {
+    const forecast = () => actions.getDailyForecast(props.city, props.days || 5);
     forecast();
   }, []);
 
@@ -36,26 +36,26 @@ const DailyCard = (props) => {
           </tr>
         </thead>
         <tbody>
-        {Array.isArray(localForecast) ? localForecast.map((localDaily, index)=>{
-          return(
-          <tr key={index}>
-            <td>
-              <p>{cleanDate(index)}</p>
-            </td>
-            <td>
-              <img alt='weather icon' src={getIcon(localDaily.weather.icon)} />
-              <p>{localDaily.weather.description}</p>
-            </td>
-            <td>
-              <p>{localDaily.temp} °C</p>
-            </td>
-            <td>
-              <p>{localDaily.min_temp + ' / ' + localDaily.max_temp}</p>
-            </td>
-            <td>
-              <p>{localDaily.dewpt}</p>
-            </td>
-          </tr>)
+          {Array.isArray(localForecast) ? localForecast.map((localDaily, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <p>{cleanDate(index)}</p>
+                </td>
+                <td>
+                  <img alt='weather icon' src={getIcon(localDaily.weather.icon)} />
+                  <p>{localDaily.weather.description}</p>
+                </td>
+                <td>
+                  <p>{localDaily.temp} °C</p>
+                </td>
+                <td>
+                  <p>{localDaily.min_temp + ' / ' + localDaily.max_temp}</p>
+                </td>
+                <td>
+                  <p>{localDaily.dewpt}</p>
+                </td>
+              </tr>)
           }) : null}
         </tbody>
       </table>
--- src/Components/HoursCard.js
+++ src/Components/HoursCard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect }  from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router';
 
 import Anchor from './Anchor';
@@ -12,12 +12,12 @@ const HoursCard = (props) => {
   const params = useParams();
 
   let localForecast = store.hourlyForecast[props.city] ? store.hourlyForecast[props.city].data : '';
-  
+
   const cleanTime = (index) => (localForecast[index].timestamp_local).split('T')[1];
   const getIcon = (icon) => 'https://www.weatherbit.io/static/img/icons/' + icon + '.png';
-  const forecast = () => actions.getHourlyForecast(props.city, props.hours || 4);
 
   useEffect(() => {
+    const forecast = () => actions.getHourlyForecast(props.city, props.hours || 4);
     forecast();
   }, []);
 
@@ -25,25 +25,25 @@ const HoursCard = (props) => {
     <Container dir='column' bgColor='#1181B2' margin='2em 1em' shadow='5px 10px 8px #888888'>
       <Heading>{props.city}</Heading>
       <Info>
-        {Array.isArray(localForecast) ? localForecast.map((localHourly, index)=>{
-        return(
-        <InfoCard key={index}>
-          <Container>
-            <h4>{cleanTime(index)}</h4>
-          </Container>
-          <Container dir='row' bgColor='#fff'>
-            <Container >
-              <img alt='weather icon' src={getIcon(localHourly.weather.icon)} />
-            </Container>
-            <Container margin='0 1.5em' dir='column' >
-              <p>{localHourly.weather.description}</p>
-              <p>{localHourly.temp}</p>
-            </Container>
-          </Container>
-        </InfoCard>)
-        }): ''}
+        {Array.isArray(localForecast) ? localForecast.map((localHourly, index) => {
+          return (
+            <InfoCard key={index}>
+              <Container>
+                <h4>{cleanTime(index)}</h4>
+              </Container>
+              <Container dir='row' bgColor='#fff'>
+                <Container >
+                  <img alt='weather icon' src={getIcon(localHourly.weather.icon)} />
+                </Container>
+                <Container margin='0 1.5em' dir='column' >
+                  <p>{localHourly.weather.description}</p>
+                  <p>{localHourly.temp}</p>
+                </Container>
+              </Container>
+            </InfoCard>)
+        }) : ''}
       </Info>
-      {params.city ? <Container minHeight='0.3em' /> : <Anchor city={props.city} /> }
+      {params.city ? <Container minHeight='0.3em' /> : <Anchor city={props.city} />}
     </Container>
   );
 };
